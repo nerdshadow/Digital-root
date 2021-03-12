40a29f17ed8749b5a061881b505bc2d825f0f312
Evilnood <evilnood666@gmail.com>
2021-03-12 13:15:12 +0200

    Добавьте файлы проекта.

---
+++ Digital root.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int digital_root(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int buff = 0;
+    while (n > 10)
+    {
+        buff = buff + n % 10;
+        n=n / 10;
+    }
+    buff = buff + n;
+    if (buff>=10)
+    {
+        digital_root(buff);
+    }
+    else
+    {
+        return buff;
+    }
+    
+}
+
+int main()
+{
+    cout << digital_root(456879);
+}
